f408bbb863adb4dad0809703b8249fd2e2e6dcb9
LordLOLQDH <adam_kraus@icloud.com>
2026-08-22 00:03:55 +0200

    Add mobile touch and pointer controls

--- controls.js
+++ controls.js
@@ -4,88 +4,80 @@ const stockfishAi = new Stockfish();
 let isDrag = false;
 let isDown = false;
 let isUp = true;
-
-
 let possibleSqres = [];
 let prevSqrIndex = null;
 let draggedPiece = null;
-let capturedPiece = 0; // it's 0 not null cause it will be applied to board.boardArr
-
+let capturedPiece = 0;
 let isStoreSqr = true;
-
-
 
 sprite.onload = () => {
     board.init();
     update();
     drawBoard();
 
-    document.addEventListener('mousedown', (e) => {
+    // Pointer Events work with mouse, touch and Apple Pencil.
+    cvs.style.touchAction = 'none';
+
+    document.addEventListener('pointerdown', (e) => {
+        if (e.pointerType === 'touch') e.preventDefault();
         isDown = true;
         isUp = false;
 
-        let mouseX = e.clientX - cvs.getBoundingClientRect().left;
-        let mouseY = e.clientY - cvs.getBoundingClientRect().top;
-
-        let boardIndex = getBoardIndex(mouseX, mouseY);
-        // if we click on a square and its not empty,
-        // we want to remove the piece from the board
-        // and store it in the draggedPiece variable
+        const rect = cvs.getBoundingClientRect();
+        const mouseX = e.clientX - rect.left;
+        const mouseY = e.clientY - rect.top;
+        const boardIndex = getBoardIndex(mouseX, mouseY);
 
         board.boardArr.forEach((sqr, i) => {
             if (sqr !== 0 && i == boardIndex) {
-
-                // but we first need to check whose turn is it first
-                // we use the whiteTurn variable and check if it corresponds with the first letter of the piece 
-
-                if ((whiteTurn && sqr.startsWith('w')) || (whiteTurn == false && sqr.startsWith('b'))) {
+                if ((whiteTurn && sqr.startsWith('w')) || (!whiteTurn && sqr.startsWith('b'))) {
                     board.boardArr[i] = 0;
                     draggedPiece = sqr;
-                    prevSqrIndex = i;// store prev square index of piece
-
+                    prevSqrIndex = i;
                     isStoreSqr = false;
-                    possibleSqres = getPossibleMoves(sqr, i); // store all possible squares to move
-
-                    highlight(possibleSqres); // highlight squares
-
+                    possibleSqres = getPossibleMoves(sqr, i);
+                    highlight(possibleSqres);
                 }
             }
-        })
-        update()
-        drawBoard()
-    })
+        });
 
-    document.addEventListener('mousemove', (e) => {
+        update();
+        drawBoard();
+    }, { passive: false });
+
+    document.addEventListener('pointermove', (e) => {
+        if (e.pointerType === 'touch') e.preventDefault();
+
         ctx.clearRect(0, 0, cvs.width, cvs.height);
         update();
 
-        let mouseX = e.clientX - cvs.getBoundingClientRect().left;
-        let mouseY = e.clientY - cvs.getBoundingClientRect().top;
+        const rect = cvs.getBoundingClientRect();
+        const mouseX = e.clientX - rect.left;
+        const mouseY = e.clientY - rect.top;
 
-        // if were dragging a piece, i.e draggedPiece !== null 
-        // we want to draw a 'fake' piece of the same type to follow the mouse
-
-        if (isDown) {
-            if (draggedPiece !== null) {
-                pieces.drawPiece(pieces.type[draggedPiece], [{ x: mouseX - pieces.pieceScale / 2, y: mouseY - pieces.pieceScale / 2 }]);
-            }
+        if (isDown && draggedPiece !== null) {
+            pieces.drawPiece(pieces.type[draggedPiece], [{
+                x: mouseX - pieces.pieceScale / 2,
+                y: mouseY - pieces.pieceScale / 2
+            }]);
         }
-    })
+    }, { passive: false });
 
-    document.addEventListener('mouseup', (e) => {
+    document.addEventListener('pointerup', (e) => {
+        if (e.pointerType === 'touch') e.preventDefault();
+
         isDown = false;
         isUp = true;
 
-        let mouseX = e.clientX - cvs.getBoundingClientRect().left;
-        let mouseY = e.clientY - cvs.getBoundingClientRect().top;
+        const rect = cvs.getBoundingClientRect();
+        const mouseX = e.clientX - rect.left;
+        const mouseY = e.clientY - rect.top;
+        const boardIndex = getBoardIndex(mouseX, mouseY);
 
-        let boardIndex = getBoardIndex(mouseX, mouseY);
-
-        // ---- utility functions ----- //
         function reverseMovement() {
             board.boardArr[prevSqrIndex] = draggedPiece;
             board.boardArr[boardIndex] = capturedPiece;
-            whiteTurn = draggedPiece[0] == 'w' ? true : false; // change the turn to the person that made the reversed move
+            whiteTurn = draggedPiece[0] == 'w';
             halfMoveCount--;
             fullMoveCount = roundToWhole(halfMoveCount / 2);
             playStockFishMove = false;
@@ -94,13 +86,13 @@ sprite.onload = () => {
         function resetMovement() {
             draggedPiece = null;
             possibleSqres = [];
+            capturedPiece = 0;
+            prevSqrIndex = null;
         }
-        // --------------------------- //
 
-        whiteDangerSqrs = []; // reset danger squares
-        blackDangerSqrs = []; // reset danger squares
+        whiteDangerSqrs = [];
+        blackDangerSqrs = [];
 
-        // check for castling movement and if it is, allow castling
         if (canWhiteCastleRightSide(prevSqrIndex, draggedPiece, boardIndex)) {
             isCastle = true;
             whiteRightSideCastle();
@@ -133,59 +125,49 @@ sprite.onload = () => {
             playStockFishMove = false;
             audio.playAudio(audio.sound.move);
         }
-        // -------------------------------------------------------- //
-
-        // once we release our piece we want to get its new location and chage it
-        // update the boardArr data and update the game
         else if (draggedPiece !== null && possibleSqres.length > 0) {
-            let isCapture = board.boardArr[boardIndex] == 0 ? false : true;
+            const isCapture = board.boardArr[boardIndex] !== 0;
+
             for (let i = 0; i < possibleSqres.length; i++) {
                 if (boardIndex == possibleSqres[i]) {
-                    capturedPiece = board.boardArr[boardIndex] !== 0 ? board.boardArr[boardIndex] : 0; // if we captured a piece
+                    capturedPiece = board.boardArr[boardIndex] !== 0 ? board.boardArr[boardIndex] : 0;
                     board.boardArr[boardIndex] = draggedPiece;
                     board.boardArr[prevSqrIndex] = 0;
-             
-                    whiteTurn = !whiteTurn; // switch turns
+
+                    whiteTurn = !whiteTurn;
                     halfMoveCount++;
                     fullMoveCount = roundToWhole(halfMoveCount / 2);
+                    playStockFishMove = whiteTurn == false;
 
-
-                    if (whiteTurn == false) playStockFishMove = true;
-                    else playStockFishMove = false;
-
-                    if (board.boardArr[boardIndex][1] == 'P') { // if it's a pawn
-                        pawnsThatHaveMovedPastOnce.push(boardIndex); // add it to the list of pawns moved more than once
+                    if (board.boardArr[boardIndex][1] == 'P') {
+                        pawnsThatHaveMovedPastOnce.push(boardIndex);
                     }
-                    // check for castling legality with every king or rook movement!
+
                     checkWhiteRightCastleLegality(prevSqrIndex, draggedPiece);
                     checkWhiteLeftCastleLegality(prevSqrIndex, draggedPiece);
                     checkBlackRightCastleLegality(prevSqrIndex, draggedPiece);
                     checkBlackLeftCastleLegality(prevSqrIndex, draggedPiece);
 
-                    // check if the king is on check
                     findWhiteDangerSqrs();
                     findBlackDangerSqrs();
 
                     if (whiteTurn == false) {
-                         // find out if the move the human made is a check
-                        if(getPossibleMoves(draggedPiece, boardIndex).includes(board.boardArr.indexOf('bK'))) {
-                            if(isCheck == false) isCheck = true;
+                        if (getPossibleMoves(draggedPiece, boardIndex).includes(board.boardArr.indexOf('bK'))) {
+                            if (isCheck == false) isCheck = true;
                         } else {
                             isCheck = false;
                         }
-                        //
-                        if (whiteDangerSqrs.includes(board.boardArr.indexOf('wK'))) { // if the current square of the wK is among the danger squares
-                            reverseMovement(); // reverse the movement until they have resolved the check
+                        if (whiteDangerSqrs.includes(board.boardArr.indexOf('wK'))) {
+                            reverseMovement();
                         }
                     } else {
                         playStockFishMove = false;
-                        if (blackDangerSqrs.includes(board.boardArr.indexOf('bK'))) { // if the current square of the bK is among the danger squares
-                            reverseMovement(); // reverse the movement until they have resolved the check
+                        if (blackDangerSqrs.includes(board.boardArr.indexOf('bK'))) {
+                            reverseMovement();
                         }
                     }
 
-                    // play the right audios
-                    if(isCheck == false) {
+                    if (isCheck == false) {
                         if (isCapture) audio.playAudio(audio.sound.capture);
                         else audio.playAudio(audio.sound.move);
                     } else {
@@ -193,18 +175,30 @@ sprite.onload = () => {
                     }
                 }
             }
-            // if the square were trying to move to isnt part of the possibleMoves
-            // we move back to the prev square
+
             if (!possibleSqres.includes(boardIndex)) {
                 board.boardArr[prevSqrIndex] = draggedPiece;
             }
             resetMovement();
-        } else if (draggedPiece !== null && possibleSqres.length <= 0) { // if there are no squares to move to
+        } else if (draggedPiece !== null) {
             board.boardArr[prevSqrIndex] = draggedPiece;
             resetMovement();
         }
+
         update();
-    })
-}
+    }, { passive: false });
 
-
+    document.addEventListener('pointercancel', () => {
+        if (draggedPiece !== null && prevSqrIndex !== null) {
+            board.boardArr[prevSqrIndex] = draggedPiece;
+        }
+        isDown = false;
+        isUp = true;
+        draggedPiece = null;
+        possibleSqres = [];
+        capturedPiece = 0;
+        prevSqrIndex = null;
+        update();
+        drawBoard();
+    });
+};
